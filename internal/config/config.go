a517dc150fc80f52edb71686fc3aaa7f9dc61783
FlakeVic <vituxa98@gmail.com>
2024-10-05 12:55:19 +0300

    enf config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	BotToken string `yaml:"bot_token"`
+	DbUrl    string `yaml:"db_url"`
+}
+
+func NewConfig() (*Config, error) {
+	var cfg Config
+	yamlFile, err := os.ReadFile("../conf.yaml")
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(yamlFile, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
